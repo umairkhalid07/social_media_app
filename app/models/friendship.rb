6d820f0dc2c88d7c97bbab7f2217bcdb4cefc3c5
umair-khalid-brainx <umair.khalid@brainxtech.com>
2022-12-05 13:08:11 +0500

    PR Fixes

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,13 +1,13 @@
 class Friendship < ApplicationRecord
-  after_create :create_inverse_relationship
-  after_destroy :destroy_inverse_relationship
-
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
   validates :user, presence: true
   validates :friend, presence: true, uniqueness: { scope: :user }
   validate :not_self
+
+  after_create :create_inverse_relationship
+  after_destroy :destroy_inverse_relationship
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,21 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :validatable
 
-  default_scope { order(name: :asc) }
+  has_one_attached :profile_photo
+  has_many :posts, dependent: :destroy
+  has_many :friend_requests, dependent: :destroy
+  has_many :pending_friends, through: :friend_requests, source: :friend
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 
   validates :name, :phone_number, :bio, :location, :birthday_at, :email, :profile_photo, presence: true
   validates :email, :phone_number, uniqueness: true
   validates :bio, length: { minimum: 15 }
   validates :phone_number, length: { is: 11 }
 
-  has_one_attached :profile_photo
+  default_scope { order(name: :asc) }
 
-  has_many :posts, dependent: :destroy
-
-  has_many :friend_requests, dependent: :destroy
-  has_many :pending_friends, through: :friend_requests, source: :friend
-
-  has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
-
-  def remove_friend(friend)
-    current_user.friends.destroy(friend)
+  def remove_friend(user, friend)
+    user.friends.destroy(friend)
   end
 end
