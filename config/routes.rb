694ca8de8e79e050552faab44bb9d5d661b83cee
umair-khalid-brainx <umair.khalid@brainxtech.com>
2022-12-05 13:09:24 +0500

    PR Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  get 'friends/index'
-  get 'friends/destroy'
+  resources :friends
 
-  get 'friends#new', to: 'friends#create'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   resources :users, only: [:index, :show]
-
-  # resources :friend_requests
 
   resources :friend_requests do
     collection do
